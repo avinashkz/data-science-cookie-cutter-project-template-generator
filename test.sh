6f4d28af426af57fa42037ca61a249134ad9a55c
ziavi7 <zi.avi7@gmail.com>
2017-11-16 18:35:18 -0800

    license try

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-echo "$(<licenses/mit.txt)"
+curl -L https://raw.githubusercontent.com/avinashkz/data-science-cookie-cutter-project-template-generator/master/licenses/apache.txt > LICENSE.ghl
+
